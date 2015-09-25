5650678996a8e3d9d06abc9cf67944551a5b1c6b
Christopher O'Brien <obriencj@gmail.com>
2015-09-25 16:28:21 -0400

    license

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,23 @@
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 3 of the
+# License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, see
+# <http://www.gnu.org/licenses/>.
+
+
 """
 Unit tests for withscope
+
+:author: Christopher O'Brien  <obriencj@gmail.com>
+:license: LGPL v.3
 """
 
 
@@ -8,6 +26,7 @@ from withscope import let
 
 
 class LetTest(TestCase):
+
 
     def test_simple_single(self):
         a = "tacos"
